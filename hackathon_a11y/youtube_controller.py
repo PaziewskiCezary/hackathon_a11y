930bebc6a568ba5608b9d4830934a2a1192ee362
Tomasz Necio <tomasz.necio@gmail.com>
2019-11-23 17:49:34 +0100

    Connected with YouTube v3 API

--- hackathon_a11y/youtube_controller.py
+++ hackathon_a11y/youtube_controller.py
@@ -1,6 +1,30 @@
+import requests
+
+from .api_key import API_KEY
+
 class YoutubeController:
     """
     Class to define methods to interact with YT API (and VLC?)
 
     """
-    pass
+    def __request(self, api, **kwargs):
+        """
+        Request to YouTube v3 API
+
+        :param api: what to put after "googleapis.com/youtube/v3/"
+        :param kwargs: request parameters, API-dependent
+        :return: Content of the response, should be a JSON
+        """
+        r = requests.get("https://www.googleapis.com/youtube/v3/{}".format(api),
+                         {
+                             "key": API_KEY,
+                             **kwargs
+                         })
+
+        return r
+
+    def search(self, phrase):
+        r = self.__request("channels", part="snippet,contentDetails,statistics",
+            id="UC_x5XG1OV2P6uZZ5FSM9Ttw")
+
+        print(r.content)
