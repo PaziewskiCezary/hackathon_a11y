4bf717d50fc68339a5329d816c603b61e2cacf3e
Tomasz Necio <tomasz.necio@gmail.com>
2019-11-24 03:21:17 +0100

    Moved images; Integrated VLC player with the rest of the project and added buttons (no working event handling yet)

--- hackathon_a11y/main_view.py
+++ hackathon_a11y/main_view.py
@@ -5,32 +5,23 @@ from tkinter import *
 from hackathon_a11y.view import View
 
 class MainView(View):
-    def add_image(self, window, filename, x, y, subsample_x, subsample_y, anchor):
-        photo = PhotoImage(master=window, file=filename)
-        photo = photo.subsample(subsample_x, subsample_y)
-        button = Button(window, image=photo)
-        button.place(relx=x, rely=y, anchor=anchor)
-        window.smieci.append(photo)
-
-
     def display(self):
-        window = Tk()
-        window.title("Odtwarzacz")
-        window.configure(background="#FAE3B4")
-        screen_width = window.winfo_screenwidth()
-        screen_height = window.winfo_screenheight()
+        self.window.title("Odtwarzacz")
+        self.window.configure(background="#FAE3B4")
+        screen_width = self.window.winfo_screenwidth()
+        screen_height = self.window.winfo_screenheight()
 
-        window.geometry(str(screen_width) + 'x' + str(screen_height))
-        window.smieci = []
+        self.window.geometry(str(screen_width) + 'x' + str(screen_height))
+        self.window.smieci = []
 
         resources_path = path.join(path.dirname(__file__), "../images/")
 
-        self.add_image(window, resources_path + "strzalka.png", 0, 0, 2, 2, 'nw')
-        self.add_image(window, resources_path + "spotify.png", 0.2, 0.5, 2, 2, CENTER)
-        self.add_image(window, resources_path + "youtube.png", 0.5, 0.5, 2, 2, CENTER)
-        self.add_image(window, resources_path + "rss.png", 0.8, 0.5, 2, 2, CENTER)
+        self.add_image(resources_path + "strzalka.png", 0, 0, 2, 2, 'nw')
+        self.add_image(resources_path + "spotify.png", 0.2, 0.5, 2, 2, CENTER)
+        self.add_image(resources_path + "youtube.png", 0.5, 0.5, 2, 2, CENTER)
+        self.add_image(resources_path + "rss.png", 0.8, 0.5, 2, 2, CENTER)
 
-        window.mainloop()
+        self.window.mainloop()
 
 if __name__ == "__main__":
     main_view = MainView()
--- hackathon_a11y/youtube_view.py
+++ hackathon_a11y/youtube_view.py
@@ -1,5 +1,15 @@
+import tkinter as tk
+from tkinter import ttk
+from os import path
+
+import vlc
+import urllib
+import pafy
+
 from hackathon_a11y.youtube_controller import YoutubeController
 from hackathon_a11y.view import View
+from hackathon_a11y.main_view import MainView
+
 
 class YoutubeView(View):
     """
@@ -9,3 +19,67 @@ class YoutubeView(View):
         controller : YouTubeController
     """
     controller = YoutubeController()
+
+    def display(self):
+        self.create_vlc_instance()
+
+        self.video_panel = ttk.Frame(self.window)
+        # self.video_panel.pack(fill=tk.BOTH, expand=True)
+        self.play_film("https://youtu.be/dzsuE5ugxf4") # DEBUG
+        self.video_panel.place(relheight=0.8, relwidth=1.0, relx=0.0, rely=0.0, anchor="nw")
+
+        resources_path = path.join(path.dirname(__file__), "../images/")
+
+        b = self.add_image(resources_path + "strzalka.png", 0.2, 1.0, 2, 2, "s")
+        self.register(b, lambda : self.change_view(MainView))
+        b = self.add_image(resources_path + "przod.png", 0.4, 1.0, 4, 4, "s")
+        self.register(b, self.stop)
+        b = self.add_image(resources_path + "pauza.png", 0.6, 1.0, 4, 4, "s")
+        self.register(b, self.pause)
+        b = self.add_image(resources_path + "start.png", 0.8, 1.0, 4, 4, "s")
+        self.register(b, self.play)
+
+        self.mainloop()
+
+    def create_vlc_instance(self):
+        self.vlc_instance = vlc.Instance()
+        self.vlc_player = self.vlc_instance.media_player_new()
+        self.window.update()
+
+    def get_handle(self):
+        return self.video_panel.winfo_id()
+
+    def play(self):
+        """Plays current file."""
+
+        if not self.vlc_player.get_media():
+            raise ValueError("No media set")
+        else:
+            if self.vlc_player.play() == -1:
+                pass
+
+    def pause(self):
+        """Pause the player."""
+        self.vlc_player.pause()
+
+    def stop(self):
+        """Stop the player."""
+        self.vlc_player.stop()
+
+    def play_film(self, file):
+        """Plays a file"""
+
+        if file.startswith('http'):
+            v = pafy.new(file)
+            v = v.getbest()
+            file = urllib.parse.unquote(v.url)
+
+        self.Media = self.vlc_instance.media_new(file)
+        self.vlc_player.set_media(self.Media)
+        self.vlc_player.set_xwindow(self.get_handle())
+        self.play()
+
+
+if __name__ == "__main__":
+    youtube_view = YoutubeView()
+    youtube_view.display()
